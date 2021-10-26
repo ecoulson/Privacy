b0c154a10c478af1857ef2709123d684cdb39cef
Evan Coulson <evan.m.coulson@gmail.com>
2021-10-26 16:27:46 -0700

    Ignoring test_utilities

--- Storage/test/test_utilities/InMemoryBucketGateway.ts
+++ Storage/test/test_utilities/InMemoryBucketGateway.ts
@@ -1,6 +1,6 @@
 import IBucket from "../../src/bucket/IBucket";
 import IBucketGateway from "../../src/bucket/IBucketGateway";
-import InMemoryGatewayUtilities from "../test_utilities/InMemoryGatewayUtilities";
+import InMemoryGatewayUtilities from "./InMemoryGatewayUtilities";
 
 export default class InMemoryBucketGateway extends InMemoryGatewayUtilities<IBucket> implements IBucketGateway {
     
