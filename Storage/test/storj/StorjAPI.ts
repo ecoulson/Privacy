2a0c3769ceea7e1724222ec147fd6a2c3ab6eeb5
Evan Coulson <evan.m.coulson@gmail.com>
2021-10-29 13:26:22 -0700

    Refactored naming and folders

--- Storage/test/storj/StorjAPI.ts
+++ Storage/test/storj/StorjAPI.ts
@@ -1,7 +1,7 @@
 import tap from "tap";
 import StorjBucket from "../../src/bucket/entities/StorjBucket";
 import StorjBucketName from "../../src/bucket/value-objects/StorjBucketName";
-import StorjAPI from "../../src/storj.api/StorjAPI";
+import StorjAPI from "../../src/storj/StorjAPI";
 import TestContextSetup from "../test_utilities/TestContextSetup";
 
 tap.beforeEach(() => {
